1b9415fee23d7ccd7aaa7d9781a175d6baad4eac
Tiago Júnio Tegon Nascimento <tiago.tegon@gmail.com>
2023-07-23 09:09:26 -0300

    feat: add menu options

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,2 +1,53 @@
 ﻿string mensagemDeBoasVindas = "Seja bem-vindo ao Screen Sound";
-Console.WriteLine(mensagemDeBoasVindas);
+
+void ExibirMensagem()
+{
+    Console.WriteLine(@"
+        ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+        ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+        ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+        ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+        ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+        ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesMenu()
+{
+    Console.WriteLine("\n--------------Menu--------------");
+    Console.WriteLine("1 - Registrar uma banda");
+    Console.WriteLine("2 - Listar todas as bandas");
+    Console.WriteLine("3 - Avaliar uma banda");
+    Console.WriteLine("4 - Exibir a média de uma banda");
+    Console.WriteLine("0 - Para sair");
+
+    Console.Write("\nSelecione uma opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch(opcaoEscolhidaNumerica)
+    {
+        case 1: 
+            Console.WriteLine("Você escolheu a opção " +  opcaoEscolhida); 
+            break;
+        case 2:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 0:
+            Console.WriteLine("Até a próxima! =)");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+ExibirMensagem();
+ExibirOpcoesMenu();
