ebcb5de092b2226d1d74208d12658dfa728068b2
Tiago Júnio Tegon Nascimento <tiago.tegon@gmail.com>
2023-07-24 21:26:52 -0300

    feat: add album class

---
+++ ScreenSound/Album.cs
@@ -0,0 +1,21 @@
+﻿class Album
+{
+    private List<Musica> musicas = new List<Musica>();
+    public string Nome { get; set; }
+    public int DuracaoTotal => musicas.Sum(musica => musica.Duracao);
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void ExibirMusicas()
+    {
+        Console.WriteLine($"Lista de músicas da banda {Nome}\n");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Música: {musica.Nome}");
+        }
+        Console.WriteLine($"\nPara ouvir esse álbum você precisa de {DuracaoTotal} segundos");
+    }
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,8 +1,14 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "Cary On";
-musica1.Artista = "Angra";
-musica1.Duracao = 275;
-musica1.Disponivel = true;
+﻿Album albumMegadeth = new Album();
 
-//musica1.ExibirFichaTecnica();
-Console.WriteLine(musica1.Descricao); 
+Musica musica1 = new Musica();
+musica1.Nome = "Tornado of Souls";
+musica1.Duracao = 50;
+
+Musica musica2 = new Musica();
+musica2.Nome = "Hangar 18";
+musica2.Duracao = 100;
+
+albumMegadeth.AdicionarMusica(musica1);
+albumMegadeth.AdicionarMusica(musica2);
+
+albumMegadeth.ExibirMusicas();
