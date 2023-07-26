158d1b1fc96375146887dd3f1bef89444ee7b651
Tiago Júnio Tegon Nascimento <tiago.tegon@gmail.com>
2023-07-25 21:01:57 -0300

    test: episode and podcast

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,24 +1,37 @@
-﻿Banda megadeth = new Banda("Megadeth");
+﻿//Banda megadeth = new Banda("Megadeth");
 
-Album albumMegadeth = new Album("Hangar 18");
+//Album albumMegadeth = new Album("Hangar 18");
 
-Musica musica1 = new Musica(megadeth, "Tornado of Souls")
-{
-    Duracao = 50,
-    Disponivel = true,
-};
+//Musica musica1 = new Musica(megadeth, "Tornado of Souls")
+//{
+//    Duracao = 50,
+//    Disponivel = true,
+//};
 
-Musica musica2 = new Musica(megadeth, "Hangar 18")
-{
-    Duracao = 100,
-    Disponivel = false,
-};
+//Musica musica2 = new Musica(megadeth, "Hangar 18")
+//{
+//    Duracao = 100,
+//    Disponivel = false,
+//};
 
-albumMegadeth.AdicionarMusica(musica1);
-albumMegadeth.AdicionarMusica(musica2);
-megadeth.AdicionarAlbum(albumMegadeth);
+//albumMegadeth.AdicionarMusica(musica1);
+//albumMegadeth.AdicionarMusica(musica2);
+//megadeth.AdicionarAlbum(albumMegadeth);
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
-albumMegadeth.ExibirMusicas();
-megadeth.ExibirDiscografia();
+//musica1.ExibirFichaTecnica();
+//musica2.ExibirFichaTecnica();
+//albumMegadeth.ExibirMusicas();
+//megadeth.ExibirDiscografia();
+
+Episodio ep1 = new Episodio(2, "Megadeth", 60);
+ep1.AdicionarConvidado("Marty Friedman");
+ep1.AdicionarConvidado("Kiko Loureiro");
+
+Episodio ep2 = new Episodio(1, "Brazil Metal", 120);
+ep2.AdicionarConvidado("Rafael Bitencourt");
+ep2.AdicionarConvidado("Gustavo Andreoli");
+
+Podcast podcast1 = new Podcast("Metal Bands", "O Cara do Metal");
+podcast1.AdicionarEpisodio(ep1);
+podcast1.AdicionarEpisodio(ep2);
+podcast1.ExibirDetalhes();
