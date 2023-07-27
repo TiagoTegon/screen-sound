73620d6c43881237acdd3fea6caf0dcef30d6e30
Tiago Júnio Tegon Nascimento <tiago.tegon@gmail.com>
2023-07-26 22:41:36 -0300

    refactor: change menus to classes

---
+++ ScreenSound/Menus/MenuSair.cs
@@ -0,0 +1,11 @@
+﻿using ScreenSound.Models;
+
+namespace ScreenSound.Menus;
+
+internal class MenuSair : Menu
+{
+    public override static void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.WriteLine("Tchau tchau :)");
+    }
+}
