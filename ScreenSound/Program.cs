c1352ca4076c108a21368f8e08b8ae78444b1f7e
Tiago Júnio Tegon Nascimento <tiago.tegon@gmail.com>
2023-07-26 21:52:20 -0300

    feat: add avaliacao class

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,6 +1,9 @@
 ﻿using ScreenSound.Models;
 
 Banda ira = new Banda("Ira!");
+ira.AdicionarNota(new Avaliacao(10));
+ira.AdicionarNota(new Avaliacao(9));
+ira.AdicionarNota(new Avaliacao(6));
 Banda beatles = new("The Beatles");
 
 Dictionary<string, Banda> bandasRegistradas = new();
@@ -138,9 +141,9 @@ void AvaliarUmaBanda()
     {
         Banda banda = bandasRegistradas[nomeDaBanda];
         Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
-        int nota = int.Parse(Console.ReadLine()!);
+        Avaliacao nota = Avaliacao.Parse(Console.ReadLine()!);
         banda.AdicionarNota(nota);
-        Console.WriteLine($"\nA nota {nota} foi registrada com sucesso para a banda {nomeDaBanda}");
+        Console.WriteLine($"\nA nota {nota.Nota} foi registrada com sucesso para a banda {nomeDaBanda}");
         Thread.Sleep(2000);
         Console.Clear();
         ExibirOpcoesDoMenu();
