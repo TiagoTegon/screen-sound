8984dd57748866ab86d88160fd2e27ecef261005
Tiago Júnio Tegon Nascimento <tiago.tegon@gmail.com>
2023-07-30 10:15:06 -0300

    feat: add discografia

--- ScreenSound/Menus/MenuExibirDetalhes.cs
+++ ScreenSound/Menus/MenuExibirDetalhes.cs
@@ -14,7 +14,12 @@ internal class MenuExibirDetalhes : Menu
         {
             Banda banda = bandasRegistradas[nomeDaBanda];
             Console.WriteLine($"\nA média da banda {banda.Nome} é {banda.Media}.");
-            Console.WriteLine("Digite uma tecla para votar ao menu principal");
+            Console.WriteLine("Discografia:");
+            foreach (Album album in banda.Albuns)
+            {
+                Console.WriteLine($"{album.Nome} -> {album.Media}");
+            }
+            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
             Console.ReadKey();
             Console.Clear();
         }
--- ScreenSound/Models/Album.cs
+++ ScreenSound/Models/Album.cs
@@ -1,8 +1,9 @@
 ﻿namespace ScreenSound.Models;
 
-internal class Album
+internal class Album : IAvaliavel
 {
     private List<Musica> musicas = new List<Musica>();
+    private List<Avaliacao> notas = new();
 
     public Album(string nome)
     {
@@ -13,6 +14,16 @@ internal class Album
     public string Nome { get; }
     public int DuracaoTotal => musicas.Sum(m => m.Duracao);
     public List<Musica> Musicas => musicas;
+
+    public double Media
+    {
+        get
+        {
+            if (notas.Count == 0) return 0;
+            return notas.Average(a => a.Nota);
+        }
+    }
+
     public static int ContadorDeObjetos = 0;
 
     public void AdicionarMusica(Musica musica)
@@ -28,5 +39,10 @@ internal class Album
             Console.WriteLine($"Música: {musica.Nome}");
         }
         Console.WriteLine($"\nPara ouvir este álbum inteiro você precisa de {DuracaoTotal}");
+    }
+
+    public void AdicionarNota(Avaliacao nota)
+    {
+        notas.Add(nota);
     }
 }
